2b97f5a570852b217f85ff1fed54a46146358b34
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-23 10:15:23 -0800

    added scaffold for form and added properties to time person class

--- MVC-TIME/Controllers/HomeController.cs
+++ MVC-TIME/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MVC_TIME.Models;
 
 namespace MVC_TIME.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int beginyear, int endyear)
+        {
+            return RedirectToAction("Results", new { beginyear, endyear });           
+        }
+        public IActionResult Results()
+        {
+            TimePerson person = new TimePerson();
+            return View(person);
         }
     }
 }
