d256921a296485c5fb253f9e4d755b485ba60420
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-23 11:44:33 -0800

    completed query

--- MVC-TIME/Controllers/HomeController.cs
+++ MVC-TIME/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MVC_TIME.Models;
 
 namespace MVC_TIME.Controllers
 {
@@ -19,10 +14,6 @@ namespace MVC_TIME.Controllers
         {
             return RedirectToAction("Results", new { beginyear, endyear });           
         }
-        public IActionResult Results()
-        {
-            TimePerson person = new TimePerson();
-            return View(person);
-        }
+      
     }
 }
--- MVC-TIME/Models/TimePerson.cs
+++ MVC-TIME/Models/TimePerson.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MVC_TIME.Models
 {
+
     public class TimePerson
     {
+
         public int Year { get; set; }
         public string Honor { get; set; }
         public string Name { get; set; }
@@ -17,6 +20,57 @@ namespace MVC_TIME.Models
         public string Category { get; set; }
         public string Context { get; set; }
 
-       
+        public TimePerson(int year, string honor, string name, string country, int birthYear, int deathYear, string title, string category, string context)
+        {
+            Year = year;
+            Honor = honor;
+            Name = name;
+            Country = country;
+            BirthYear = birthYear;
+            DeathYear = deathYear;
+            Title = title;
+            Category = category;
+            Context = context;
+        }
+
+        public List<TimePerson> GetPerson(int startyear, int endyear)
+        {
+            string _filePath = @"C:\Code\Lab 11-My First MVC App\Lab11-MyFirstMVC_App\MVC-TIME\wwwroot\PersonOfTheYear.csv";
+
+
+            List<string> allPeople = LoadCsvFile(_filePath);
+            // This is strings List of csv file
+
+            // This is TimePerson List of query
+            var query = from line in allPeople
+                        let data = line.Split(',')
+                        select new TimePerson(Int32.Parse(data[0]), data[1], data[2], data[3], Int32.Parse(data[4]), Int32.Parse(data[5]), data[6], data[7], data[98]);
+
+            List<TimePerson> requestedPeople = new List<TimePerson>();
+
+            foreach (TimePerson person in query)
+            {
+                if (person.Year >= startyear || person.Year <= endyear)
+                    requestedPeople.Add(person);
+            }
+
+            return requestedPeople;
+        }
+
+        List<String> LoadCsvFile(string _filePath)
+        {
+            var reader = new StreamReader(File.OpenRead(_filePath));
+            List<String> searchList = new List<String>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                searchList.Add(line);
+            }
+            return searchList;
+        }
+
     }
+
+}
+
 }
